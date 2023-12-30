70f61945ab1e24a6b8da762c6032f12fe662da41
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-12-30 09:43:33 +0530

    add initial setup of the worldwise project with react and vite

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const HomePage = () => {
+  return (
+    <div>
+      <PageNav />
+      <h2>HomePage</h2>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const Pricing = () => {
+  return (
+    <div>
+      <PageNav />
+      <h2>Pricing</h2>
+    </div>
+  );
+};
+
+export default Pricing;
